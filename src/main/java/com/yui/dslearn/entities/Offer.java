a20022d4e3844bb05a0023dc57c7069dd30cd9ea
rianfernandesno <zrpapz12@gmail.com>
2025-05-23 19:28:22 -0300

    Resource

--- src/main/java/com/yui/dslearn/entities/Offer.java
+++ src/main/java/com/yui/dslearn/entities/Offer.java
@@ -3,6 +3,8 @@ package com.yui.dslearn.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -19,6 +21,9 @@ public class Offer {
     @ManyToOne
     @JoinColumn(name = "course_id")
     private Course course;
+
+    @OneToMany(mappedBy = "offer")
+    private List<Resource> resources = new ArrayList<>();
 
     public Offer(){}
 
@@ -44,6 +49,10 @@ public class Offer {
 
     public Course getCourse() {
         return course;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
     }
 
     public void setEdition(String edition) {
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -22,6 +22,11 @@ INSERT INTO tb_offer(edition, start_Moment, end_Moment, course_id) VALUES ('1.0'
 INSERT INTO tb_offer(edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2025-12-20T03:00:00', TIMESTAMP WITH TIME ZONE '2026-12-20T03:00:00', 1);
 
 
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES('Trilha HTML', 'Trilha principal do curso', 1,'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 1, 1)
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES('Forum', 'Tire suas duvidas', 2,'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 2, 1)
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES('Lives', 'Lives exclusivas para a turma', 3,'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 0, 1)
+
+
 
 
 
