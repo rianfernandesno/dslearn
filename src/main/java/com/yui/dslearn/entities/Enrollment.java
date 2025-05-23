00f67dda4b9b953054290cac1731dd2bec3f7fcf
rianfernandesno <zrpapz12@gmail.com>
2025-05-23 20:20:24 -0300

    Enrollment

---
+++ src/main/java/com/yui/dslearn/entities/Enrollment.java
@@ -0,0 +1,99 @@
+package com.yui.dslearn.entities;
+
+import com.yui.dslearn.entities.pk.EnrollmentPk;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+    @EmbeddedId
+    private EnrollmentPk id = new EnrollmentPk();
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant enrollMoment;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant refundMoment;
+
+    private boolean available;
+    private boolean onlyUpdate;
+
+    public Enrollment(){}
+
+    public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available, boolean onlyUpdate) {
+        id.setUser(user);
+        id.setOffer(offer);
+        this.enrollMoment = enrollMoment;
+        this.refundMoment = refundMoment;
+        this.available = available;
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    public User getStudent(){
+        return  id.getUser();
+    }
+
+    public void  setStudent(User user){
+        id.setUser(user);
+    }
+
+    public Offer getOffer(){
+        return id.getOffer();
+    }
+
+    public void setOffer(Offer offer){
+        id.setOffer(offer);
+    }
+
+    public Instant getEnrollMoment() {
+        return enrollMoment;
+    }
+
+    public void setEnrollMoment(Instant enrollMoment) {
+        this.enrollMoment = enrollMoment;
+    }
+
+    public Instant getRefundMoment() {
+        return refundMoment;
+    }
+
+    public void setRefundMoment(Instant refundMoment) {
+        this.refundMoment = refundMoment;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public boolean isOnlyUpdate() {
+        return onlyUpdate;
+    }
+
+    public void setOnlyUpdate(boolean onlyUpdate) {
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Enrollment that = (Enrollment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/yui/dslearn/entities/pk/EnrollmentPk.java
@@ -0,0 +1,56 @@
+package com.yui.dslearn.entities.pk;
+
+import com.yui.dslearn.entities.Offer;
+import com.yui.dslearn.entities.User;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable
+public class EnrollmentPk {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    public  EnrollmentPk(){}
+
+    public EnrollmentPk(User user, Offer offer) {
+        this.user = user;
+        this.offer = offer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EnrollmentPk that = (EnrollmentPk) o;
+        return Objects.equals(user, that.user) && Objects.equals(offer, that.offer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, offer);
+    }
+}
