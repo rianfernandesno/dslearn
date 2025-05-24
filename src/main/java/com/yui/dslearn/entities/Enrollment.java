57cfd35d5dd346cae05b2800ff7113c0c62ed5a0
rianfernandesno <zrpapz12@gmail.com>
2025-05-23 23:06:07 -0300

    Lessons seed, fix: lessonsDone

--- src/main/java/com/yui/dslearn/entities/Enrollment.java
+++ src/main/java/com/yui/dslearn/entities/Enrollment.java
@@ -1,13 +1,12 @@
 package com.yui.dslearn.entities;
 
 import com.yui.dslearn.entities.pk.EnrollmentPk;
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_enrollment")
@@ -24,6 +23,9 @@ public class Enrollment {
 
     private boolean available;
     private boolean onlyUpdate;
+
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    private Set<Lesson> lessons = new HashSet<>();
 
     public Enrollment(){}
 
